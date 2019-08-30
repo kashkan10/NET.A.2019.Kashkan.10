37e500ec3c70b681e1ce543004debb23a0a22aea
kashkan10 <a.kashkan10@gmail.com>
2019-08-31 00:27:39 +0300

    update

---
+++ Day10Tasks/JaggedArraySort/Comparers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JaggedArraySort
+{
+    public class SumOfSort : IComparer<int[]>
+    {
+        /// <summary>
+        /// IComparer implementation(sum of elements)
+        /// </summary>
+        /// <param name="a">First array</param>
+        /// <param name="b">Second array</param>
+        /// <returns>result</returns>
+        public int Compare(int[] a, int[] b)
+        {
+            return a.Sum().CompareTo(b.Sum());
+        }
+    }
+
+    public class MaxOfSort : IComparer<int[]>
+    {
+        /// <summary>
+        /// IComparer implementation(max of elements)
+        /// </summary>
+        /// <param name="a">First array</param>
+        /// <param name="b">Second array</param>
+        /// <returns>result</returns>
+        public int Compare(int[] a, int[] b)
+        {
+            return a.Max().CompareTo(b.Max());
+        }
+    }
+
+    public class MinOfSort : IComparer<int[]>
+    {
+        /// <summary>
+        /// IComparer implementation(min of elements)
+        /// </summary>
+        /// <param name="a">First array</param>
+        /// <param name="b">Second array</param>
+        /// <returns>result</returns>
+        public int Compare(int[] a, int[] b)
+        {
+            return a.Min().CompareTo(b.Min());
+        }
+    }
+}
---
+++ Day10Tasks/JaggedArraySort/JaggedSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaggedArraySort
+{
+    public class JaggedSort
+    {
+        /// <summary>
+        /// Comparison method with IComparer as parameter
+        /// </summary>
+        /// <param name="arr">Jagged array</param>
+        /// <param name="comparer"></param>
+        public void Compare(int[][] arr, IComparer<int[]> comparer)
+        {
+            Sort(arr, comparer.Compare);
+        }
+
+        /// <summary>
+        /// Comparison method with delegate as parameter
+        /// </summary>
+        /// <param name="arr">Jagged array</param>
+        /// <param name="comparer">Second array</param>
+        public void Sort(int[][] arr, Comparison<int[]> comparer)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (comparer(arr[i], arr[j]) > 0)
+                    {
+                        int[] swap = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swap;
+                    }
+                }
+            }
+        }
+    }
+}
